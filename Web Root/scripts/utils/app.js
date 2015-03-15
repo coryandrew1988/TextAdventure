2c570cf29d6a4adfc2d6d2df3cc7faa77ffe02b5
Cory <cory.andrew.1988@gmail.com>
2015-03-15 00:35:14 -0400

    Added a rudimentary map, player, and look-around.

--- Web Root/scripts/utils/app.js
+++ Web Root/scripts/utils/app.js
@@ -3,16 +3,46 @@ define(['lib/index'], function (lib) {
     var $ = lib.$;
     var ko = lib.ko;
     
-    var game = {
+    var terrainTypes = [{
+        name: 'grassland'
+    }, {
+        name: 'road'
+    }, {
+        name: 'forest'
+    }];
+    
+    var map = {
+        width: 16,
+        height: 16,
+        grid: []
+    };
+    
+    _.each(_.range(0, map.width), function (i) {
+        map.grid[i] = [];
+        _.each(_.range(0, map.height), function (j) {
+            map.grid[i][j] = {
+                terrainType: terrainTypes[Math.random() * terrainTypes.length | 0],
+                objects: [],
+                describe: function () {
+                    return 'It\'s ' + this.terrainType.name + '!';
+                }
+            };
+        });
+    });
+    
+    var player = {
+        x: 0,
+        y: 0,
+        look: function () {
+            return { text: map.grid[player.x][player.y].describe() };
+        }
+    };
+    
+    var gameUI = {
         outputMessages: ko.observableArray([{ text: 'Some sample output.' }]),
         inputText: ko.observable('Some sample input.'),
         processInput: function () {
-            var inputText = game.inputText();
-            game.inputText('');
-
-            game.outputMessages.push({
-                text: 'You said: ' + inputText
-            });
+            game.update();
         },
         afterRenderOutputMessages: function () {
             var outputElement = $('.output')[0];
@@ -20,16 +50,32 @@ define(['lib/index'], function (lib) {
         }
     };
     
+    var game = {
+        map: map,
+        read: function () {
+            var inputText = gameUI.inputText();
+            gameUI.inputText('');
+            return inputText;
+        },
+        write: function (message) {
+            gameUI.outputMessages.push(message);
+        },
+        update: function () {
+            game.write(player.look());
+            game.write({ text: 'You said: ' + game.read() });
+        }
+    };
+    
     $(window).on('keyup', function (event) {
         if (event.keyCode === 13) {
-            game.processInput();
+            gameUI.processInput();
         }
     });
 
     return {
         template: {
             name: 'mainTemplate',
-            data: game
+            data: gameUI
         }
     };
 });
